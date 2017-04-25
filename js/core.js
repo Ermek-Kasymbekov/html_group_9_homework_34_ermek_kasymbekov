ed20449856a6952d27f2a51ed773aa1e221f9669
Ermek Kasymbekov <fabolopfb@gmail.com>
2017-04-25 18:02:57 +0600

    #7

--- js/core.js
+++ js/core.js
@@ -2,30 +2,44 @@ $(document).ready(function(){
     $("[data-fancybox]").fancybox({});
 
 
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > $('header').height()) {
+            $('.top_menu').addClass('fixed');
+        } else {
+            $('.top_menu').removeClass('fixed');
+        }
+    });
+
+
+
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        console.log(id);
+        console.log(top);
+        $('body,html').animate({scrollTop: top - $('#top_menu').height()}, 1500);
+    });
+
+
+
+    $('#menu-btn').click(function(){
+        $('.menu').slideToggle(700);
+        if ($(this).hasClass('not-active')) {
+            $(this).addClass('is-active').removeClass('not-active');
+        }else{
+            setTimeout(function(){
+                $('.is-active').addClass('not-active').removeClass('is-active')
+            },600)
+        }
+    });
+
+
+
 });
 
 
-$(window).scroll(function(){
-    if ($(this).scrollTop() > 450) {
-        $('.top_menu').addClass('fixed');
-    } else {
-        $('.top_menu').removeClass('fixed');
-    }
-});
+// lToAnchor('id3');
 
-//
-// $(window).scroll(function(){
-//     if ($(this).scrollTop() > 300) {
-//         $('.top_menu').addClass('fixed');
-//     } else {
-//         $('.top_menu').removeClass('fixed');
-//     }
-// });
 
-//
-// function scrollToAnchor(aid){
-//     var aTag = $("a[name='"+ aid +"']");
-//     $('html,body').animate({scrollTop: aTag.offset().top},'slow');
-// }
-//
-// scrollToAnchor('id3');
+
